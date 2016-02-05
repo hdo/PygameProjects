d8202d33e0954123319bca51ff99b083134c47b8
hdo <hdo@hdo-ThinkPad-T420>
2016-02-05 20:45:57 +0100

    add scrolling to menu (scope handling)
    add pygame.clock

--- menutest/menu01.py
+++ menutest/menu01.py
@@ -17,33 +17,50 @@ class SimpleMenu:
         self.selectedtextcolor = self.textcolor
         self.canvas = pygame.Surface((self.width, self.height))
         self.textfont = pygame.font.Font(None, self.text_height)
+        self.show_max = self.height / self.item_height
+        #self.show_max = 5
+        self.show_scope = (0, self.show_max-1)
         
     def add_item(self, title):
         self.menuitems.append(title)
 
+    def update_scope(self):
+        if self.index < self.show_scope[0]:
+            # TODO add sanity check
+            self.show_scope = (self.show_scope[0]-1, self.show_scope[1]-1)
+        if self.index > self.show_scope[1]:
+            # TODO add sanity check
+            self.show_scope = (self.show_scope[0]+1, self.show_scope[1]+1)
+            print self.show_scope
+                
+
     def inc_index(self):
         if self.index < len(self.menuitems) - 1:
             self.index = self.index + 1
+            self.update_scope()
             
     def dec_index(self):
         if self.index > 0:
             self.index = self.index - 1
-            
+            self.update_scope()
 
     def get_menu(self):
         self.canvas.fill(self.background)
         counter = 0
-        for title in self.menuitems:
+        for i in range(self.show_scope[0], self.show_scope[1]+1):
+            title = self.menuitems[i]
+        #for title in self.menuitems:
             sf = pygame.Surface((self.width, self.item_height))
             text = self.textfont.render(title, 1, self.textcolor)
             textpos = text.get_rect()
             textpos.centery = sf.get_rect().centery+2
             textpos.x = 5
-            if self.index == counter:
+            if self.index == i:
                 pygame.draw.rect(sf, self.selectedborder, (1,1,self.width-2, self.item_height-2), 2)
             sf.blit(text, textpos)
-            self.canvas.blit(sf, (0, 10 + counter*(self.item_height)))
+            self.canvas.blit(sf, (0, 1 + counter*(self.item_height)))
             counter = counter + 1
+        pygame.draw.rect(self.canvas, (255,0,0), (0, 0, self.width, self.height), 2)
         return self.canvas
     
 def create_screen():
@@ -65,34 +82,17 @@ def create_screen():
                 continue
             found = True
             break
-        return pygame.display.set_mode((800, 480), pygame.FULLSCREEN)
+        #return pygame.display.set_mode((800, 480), pygame.FULLSCREEN)
+        return pygame.display.set_mode((800, 480))
     
 pygame.init()
 screen = create_screen()
+clock = pygame.time.Clock()
 
 done = False
 
-def get_item(title):
-    item_dim = (300, 30)
-    font = pygame.font.Font(None, 30)
-    text = font.render(title, 1, (255, 255, 255))
-    textpos = text.get_rect()
-    sf = pygame.Surface(item_dim)
-    print textpos
-    textpos.centery = sf.get_rect().centery+2
-    textpos.x = 5
-    pygame.draw.rect(sf, (255,255,0), (1,1,item_dim[0]-2, item_dim[1]-2), 2)
-    sf.blit(text, textpos)
-    return sf
-
-
-#screen.blit(get_item("MENU ENTRY 01"), (40,10))
-#screen.blit(get_item("MENU ENTRY 02"), (40,50))
-#screen.blit(get_item("MENU ENTRY 03"), (40,90))
-#screen.blit(get_item("MENU ENTRY 04"), (40,130))
-
 menu = SimpleMenu(380, 400, 30)
-for i in range(10):
+for i in range(50):
     menu.add_item("MENU ENTRY %.3d" % i)
 menu.index = 4
 screen.blit(menu.get_menu(), (300,10))
@@ -120,3 +120,5 @@ while not done:
         screen.fill((0,0,0))
         screen.blit(menu.get_menu(), (400,50))
         pygame.display.flip()
+        
+    clock.tick(30)
