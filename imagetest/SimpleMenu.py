7a725abf36a6f5fb5b76bd115cf2b7d15e7457a1
hdo <hdo@hdo-ThinkPad-T420>
2016-02-05 22:02:20 +0100

    add title

---
+++ imagetest/SimpleMenu.py
@@ -0,0 +1,63 @@
+import pygame
+
+class SimpleMenu:
+    
+    def __init__(self, width, height, item_height):
+        self.menuitems = []
+        self.index = 0
+        self.width = width
+        self.height = height
+        self.item_height = item_height
+        self.text_height = self.item_height
+        self.background = (0,0,0) # black
+        self.textcolor = (235, 235, 235) # white
+        #self.selectedborder = (255, 255, 0) # yellow
+        self.selectedborder = (0, 255, 0) # green
+        self.selectedtextcolor = self.textcolor
+        self.canvas = pygame.Surface((self.width, self.height))
+        self.textfont = pygame.font.Font(None, self.text_height)
+        self.show_max = self.height / self.item_height
+        #self.show_max = 5
+        self.show_scope = (0, self.show_max-1)
+        
+    def add_item(self, title):
+        self.menuitems.append(title)
+
+    def update_scope(self):
+        if self.index < self.show_scope[0]:
+            # TODO add sanity check
+            self.show_scope = (self.show_scope[0]-1, self.show_scope[1]-1)
+        if self.index > self.show_scope[1]:
+            # TODO add sanity check
+            self.show_scope = (self.show_scope[0]+1, self.show_scope[1]+1)
+            print self.show_scope
+                
+
+    def inc_index(self):
+        if self.index < len(self.menuitems) - 1:
+            self.index = self.index + 1
+            self.update_scope()
+            
+    def dec_index(self):
+        if self.index > 0:
+            self.index = self.index - 1
+            self.update_scope()
+
+    def get_menu(self):
+        self.canvas.fill(self.background)
+        counter = 0
+        for i in range(self.show_scope[0], self.show_scope[1]+1):
+            title = self.menuitems[i]
+        #for title in self.menuitems:
+            sf = pygame.Surface((self.width, self.item_height))
+            text = self.textfont.render(title, 1, self.textcolor)
+            textpos = text.get_rect()
+            textpos.centery = sf.get_rect().centery+2
+            textpos.x = 5
+            if self.index == i:
+                pygame.draw.rect(sf, self.selectedborder, (1,1,self.width-2, self.item_height-2), 2)
+            sf.blit(text, textpos)
+            self.canvas.blit(sf, (0, 1 + counter*(self.item_height)))
+            counter = counter + 1
+        #pygame.draw.rect(self.canvas, (255,0,0), (0, 0, self.width, self.height), 2)
+        return self.canvas
--- imagetest/showcover.py
+++ imagetest/showcover.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+import SimpleMenu
 
 def create_screen():
     if os.name == "nt":
@@ -19,10 +19,22 @@ def create_screen():
                 continue
             found = True
             break
-        return pygame.display.set_mode((800, 480), pygame.FULLSCREEN)
+        #return pygame.display.set_mode((800, 480), pygame.FULLSCREEN)
+        return pygame.display.set_mode((800, 480))
+
+
+
+def get_title(title):
+    textfont = pygame.font.Font(None, 40)    
+    text = textfont.render(title, 1, (200,200,200))
+    return text
+    
+    
+    
+    
     
 pygame.init()
-gameDisplay = create_screen()
+screen = create_screen()
 
 pygame.display.flip()
 pygame.display.update()
@@ -39,12 +51,24 @@ cover_list = []
 for item in sorted(os.listdir('cover')):
     fullname = os.path.join('cover',item)
     current_cover = pygame.image.load(fullname)
+    current_cover = pygame.transform.scale(current_cover, (360, 360))
     cover_list.append(current_cover)
+for item in sorted(os.listdir('cover')):
+    fullname = os.path.join('cover',item)
+    current_cover = pygame.image.load(fullname)
+    current_cover = pygame.transform.scale(current_cover, (360, 360))
+    cover_list.append(current_cover)
+
+menu = SimpleMenu.SimpleMenu(380, 400, 30)
+for i in range(50):
+    menu.add_item("MENU ENTRY %.3d" % i)
+menu.textcolor = (200,200,200)    
+    
 
 
 current_index = 0
+dirty = True
 while not gameExit:
-    gameDisplay.blit(cover_list[current_index], cover_list[current_index].get_rect())
 
     for event in pygame.event.get():
         #print(event)
@@ -52,17 +76,30 @@ while not gameExit:
             if event.key == pygame.K_ESCAPE:
                 gameExit = True
             if event.key == pygame.K_1:
-                gameDisplay.fill((255,0,0))
+                screen.fill((255,0,0))
             if event.key == pygame.K_2:
-                gameDisplay.fill((0,255,0))
+                screen.fill((0,255,0))
             if event.key == pygame.K_3:
-                gameDisplay.fill((0,0,255))
-            if event.key == pygame.K_UP:
+                screen.fill((0,0,255))
+            if event.key == pygame.K_DOWN:
                 if current_index < len(cover_list)-1:
                     current_index = current_index + 1
-            if event.key == pygame.K_DOWN:
+                    menu.inc_index()
+                    dirty = True
+            if event.key == pygame.K_UP:
                 if current_index > 0:
                     current_index = current_index -1 
+                    menu.dec_index()
+                    dirty = True
+    if dirty:
+        dirty = False
+        screen.blit(menu.get_menu(), (400,60))
+        new_pos = cover_list[current_index].get_rect()
+        new_pos.centerx = 200
+        new_pos.centery = 240
+        screen.blit(cover_list[current_index], new_pos)
+        screen.blit(get_title(menu.menuitems[menu.index]), (50,10))
+        
 
     pygame.display.update()
     clock.tick(30)
